3f8e10027d8881d5c02b795b3b6f3308e541aa85
Hillo111 <stas.gannutin@gmail.com>
2021-07-17 19:27:38 +0300

    Finish games

--- escape-room/games/sweep.py
+++ escape-room/games/sweep.py
@@ -1,73 +1,67 @@
-from asciimatics.screen import Screen, KeyboardEvent, MouseEvent
-from asciimatics.scene import Scene
-from asciimatics.effects import Cycle, Stars
-from asciimatics.renderers import FigletText
-from time import sleep
+from asciimatics.screen import Screen
 import sys
-from game_utils import display_dialogue, generate_person
-import curses
-from sweep_games import game1, game2, game3
-
-if sys.platform == 'darwin':
-    window_title = 'Terminal'
-elif sys.platform == 'win32':
-    window_title == 'python3 sweep.py'
-# TODO: Include check for linux distros
+from game_utils import generate_person, set_screen_size, display_dialogue
+from sweep_games import game1, game2
 
 worlds = []
 
-curses.initscr()
-curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+game_fin = False
 
 def demo(screen):
-    print('\033[?1003h') # enable mouse tracking with the XTERM API
+    global game_fin
+
+    tw, th = 150, 40
+    l = 0
+
+    if abs(screen.width - tw) > l or abs(screen.height - th) > l:
+        set_screen_size(screen, (tw, th), leniency=l)
+        return
 
     texts = [
         'Welcome, janitor, to your first and last day at this nuclear power plant!',
-        'You\'ll have three tasks today, after which everyone gets to go home.',
+        'You\'ll have two tasks today, both involving shooting bugs or something, after which everyone gets to go home.',
         'Understood? Good, now get to work.',
-        'Well done. Truly brilliant. Your scrubbing was incredible.',
+        'Well done. Truly brilliant. Your noscoping was incredible.',
         'Damn it, those children are back. I really hope they don\'t-',
         'Well, it appears the children have stolen your legs. No good. But hey, you still '\
-        'have arms. We\'ll mount you on a robotic frame and you\'ll get to work.',
+        'have arms.',
+        'We\'ll mount you on a robotic frame and you\'ll do more of the same, but not, or something',
         'Nicely done. Now, there\'s one more thing-',
         'Oh no, the children. It would sure be a shame if they-',
         'Good news? The status of your legs has not changed. Bad news? Your '\
         'arms have mysteriously disappeared.',
-        'But hey, no problem, you still have your BrainInABox™. Get to work.',
+        'You still have to do your job though, so here\'s a shotgun. Yeah, it\'s '\
+        'pretty small, but you\'ll figure something out',
         'Well, uh, I guess you\'re done for the day. Kind of strange how you\'re '\
         'getting paid what, $960,781,132 for a one-time janitorial job? Honestly, '\
         'this sounds like a money-laundering sch-',
         '...',
         'Uh, I\'ll be off. Have a good day sir.'
     ]
-
+    '''
     sprite_sequence = {
         0: [0.45, generate_person(), 0.1, generate_person()],
-        4: [0.2, generate_person(small=True), generate_person(small=True), 0.25,
+        5: [0.2, generate_person(small=True), generate_person(small=True), 0.25,
             generate_person(), 0.1, generate_person()],
-        5: [0.1, generate_person(small=True), generate_person(small=True), 0.35,
+        6: [0.1, generate_person(small=True), generate_person(small=True), 0.35,
             generate_person(legs=False), 0.1, generate_person()],
-        6: [0.45, generate_person(legs=False), 0.1, generate_person()],
-        7: [0.2, generate_person(small=True), generate_person(small=True), 0.25,
+        7: [0.45, generate_person(legs=False), 0.1, generate_person()],
+        8: [0.2, generate_person(small=True), generate_person(small=True), 0.25,
             generate_person(legs=False), 0.1, generate_person()],
-        8: [0.1, generate_person(small=True), generate_person(small=True), 0.35,
+        9: [0.1, generate_person(small=True), generate_person(small=True), 0.35,
             generate_person(legs=False, arms=False), 0.1, generate_person()],
-        9: [0.45, generate_person(legs=False, arms=False), 0.1, generate_person()],
+        10: [0.45, generate_person(legs=False, arms=False), 0.1, generate_person()],
     }
-
+    '''
     screen.clear()
-    display_dialogue(screen, texts)
+    display_dialogue(screen, texts[0:3], box_dimensions=(50, 6))
+    game1(screen)
+    display_dialogue(screen, texts[3:7], box_dimensions=(50, 6))
+    game2(screen)
+    display_dialogue(screen, texts[12:], box_dimensions=(50, 6))
+    # game3(screen)
 
+    game_fin = True
 
-
-
-    c = 0
-    play = True
-    while play:
-        
-
-        
-        sleep(1 / 60)
-
-Screen.wrapper(demo)
+while not game_fin: 
+    Screen.wrapper(demo)
